c0c08ecbce515cbeee682adfc7dc7bfbc225f6ee
Jason Aurich <jason@refund-retriever.com>
2016-09-11 19:55:42 -0500

    Update to number conversion in calculator.rb

--- calculator.rb
+++ calculator.rb
@@ -90,15 +90,18 @@ loop do
 
   operation_input
 
+  @number1 = @number1.to_f
+  @number2 = @number2.to_f
+
   result =  case @operation
             when '1'
-              @number1.to_f + @number2.to_f
+              @number1 + @number2
             when '2'
-              @number1.to_f - @number2.to_f
+              @number1 - @number2
             when '3'
-              @number1.to_f * @number2.to_f
+              @number1 * @number2
             when '4'
-              @number1.to_f / @number2.to_f
+              @number1 / @number2
             end
 
   if integer?(result)
